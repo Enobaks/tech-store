ec40a7bcfcbbbf5efc7d20a60a193c369330b558
Emmanuel <enobakhare.emmanuel@gmail.com>
2022-08-08 17:59:26 +0100

    updated side Navbar on user dashboard

--- src/components/sideNavBar/sideNavBar.js
+++ src/components/sideNavBar/sideNavBar.js
@@ -4,13 +4,37 @@ import logo from "../../images/ts_logo_icon.png";
 
 const sideNavBar = () => {
   return (
-    <div className="sideNavContainer w-80 bg-orange-300">
-      <Link to="/" className="logo flex">
+    <div className="sideNavContainer w-80 bg-white z-20 drop-shadow-md flex flex-col items-center justify-between">
+      <Link to="/" className="logo flex mt-6 mb-5">
         <img src={logo} alt="Logo" className="!w-8 !h-8" />
         <span className="md:block  hidden text-3xl font-medium">
           Tech Store
         </span>
       </Link>
+      <hr className="w-full" />
+      <ul className="nat w-full h-full flex flex-col items-center pt-16">
+        <Link to="" className="w-full mb-5">
+          <li className="text-center">
+            {" "}
+            <i class="fa-regular fa-user mr-2 text-lg"></i>Profile
+          </li>
+        </Link>
+        <Link to="" className="w-full mb-5">
+          <li className="text-center">
+            <i class="fa-solid fa-cart-arrow-down mr-2 text-lg"></i>Orders
+          </li>
+        </Link>
+        <Link to="" className="w-full mb-5">
+          <li className="text-center">
+            <i class="fa-regular fa-heart mr-2 text-lg"></i>Saved
+          </li>
+        </Link>
+        <Link to="" className="w-full mb-5">
+          <li className="text-center">
+            <i class="fa-solid fa-right-from-bracket mr-2 text-lg"></i>Logout
+          </li>
+        </Link>
+      </ul>
     </div>
   );
 };
---
+++ src/pages/saved_items.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const saved_items = () => {
+  return <div>saved_items</div>;
+};
+
+export default saved_items;
